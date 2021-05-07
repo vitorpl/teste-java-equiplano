e10f7da3f9a9ddf7b8642771ab7c4a2308c6fe3e
Vitor Pastuch Lazarotto <vitor.lazarotto@gmail.com>
2021-05-07 10:34:00 -0300

    nova geracao do jar e war com o swagger

--- segplano-ui/src/app/util/util.ts
+++ segplano-ui/src/app/util/util.ts
@@ -14,15 +14,16 @@ export class Util {
     }
 
     public static isCPFValido(cpf: string): boolean {
-
-        cpf = cpf.replace(/[^0-9]/g,'');
-
+        
         if (cpf == null) {
             return false;
         }
         if (cpf.length != 11) {
             return false;
         }
+        
+        cpf = cpf.replace(/[^0-9]/g,'');
+
         if ((cpf == '00000000000') || (cpf == '11111111111') || (cpf == '22222222222') || (cpf == '33333333333') || (cpf == '44444444444') || (cpf == '55555555555') || (cpf == '66666666666') || (cpf == '77777777777') || (cpf == '88888888888') || (cpf == '99999999999')) {
             return false;
         }
